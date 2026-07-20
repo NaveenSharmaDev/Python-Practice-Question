80d4c6531ce5df219522159bef65823d58bf9356
Naveen Sharma <naveensharmaaurangabad@gmail.com>
2026-07-20 23:42:07 +0530

    Add files via upload

---
+++ Checknum.py
@@ -0,0 +1,9 @@
+#check number is positiver and negative or Zero
+
+num = float(input("Enter a number: "))
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
---
+++ Checkvowel.py
@@ -0,0 +1,5 @@
+char = input("Enter a character: ")
+if char in ("a","e","i","o","u"):
+    print(char, "is a vowel.")
+else:
+    print(char, "is not a vowel.")
